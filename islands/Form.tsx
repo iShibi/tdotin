7ab9c07aa0728a278c079bd2fad22e9a1954560b
Shubham Parihar <shubhamparihar391@gmail.com>
2024-03-10 09:28:23 +0530

    feat: add option to set link expiry date

--- islands/Form.tsx
+++ islands/Form.tsx
@@ -1,13 +1,36 @@
 import { useState } from "preact/hooks";
 
+const expireAfterOptions = {
+  "30 minutes": 30,
+  "1 hour": 60,
+  "6 hours": 360,
+  "12 hours": 720,
+  "1 day": 1440,
+  "7 days": 10080,
+};
+
 export default function Form() {
   const [url, setUrl] = useState<string>("");
   const [shortUrl, setShortUrl] = useState<string>("");
+  const [expireAfter, setExpireAfter] = useState("never");
+  const [expireDate, setExpireDate] = useState("");
+  const [expireTime, setExpireTime] = useState("");
 
   const store = async () => {
+    let expireTimestamp;
+    if (expireAfter === "never") {
+      expireTimestamp = null;
+    } else if (expireAfter === "custom") {
+      expireTimestamp = new Date(`${expireDate}T${expireTime}`).getTime();
+    } else {
+      expireTimestamp = new Date(
+        new Date().getTime() + Number.parseInt(expireAfter) * 60000,
+      )
+        .getTime();
+    }
     const res = await fetch(location.href + "api/url", {
       method: "POST",
-      body: JSON.stringify({ url }),
+      body: JSON.stringify({ url, expireTimestamp }),
     });
     const data = await res.json() as { alias: string; url: string };
     setShortUrl(`${location.href}${data.alias}`);
@@ -26,19 +49,63 @@ export default function Form() {
           class="rounded-md px-2 hover:outline-none focus:outline-none shadow-lg py-2 border-dotted border-black border-2 w-full"
         />
         <div class="mr-auto gap-2 flex flex-col">
-          <label
-            htmlFor="expires_at"
-            class=""
-          >
-            Expires At
-          </label>
-          <input
-            type="date"
-            name="expires_at"
-            id="expires_at"
+          <label htmlFor="">Expire After</label>
+          <select
             class="border-dotted border-black border-2 shadow-lg w-fit px-2 py-1 rounded-md"
-          />
+            name="expire_after"
+            id="expire_after"
+            onChange={(e) =>
+              setExpireAfter((e.target as HTMLInputElement).value)}
+          >
+            <option value="never">Never</option>
+            {Object.entries(expireAfterOptions).map(([key, value]) => (
+              <option key={key} value={value}>{key}</option>
+            ))}
+            <option value="custom">Custom</option>
+          </select>
         </div>
+        {expireAfter === "custom"
+          ? (
+            <div class="mr-auto gap-2 flex flex-row">
+              <div class="flex flex-col">
+                <label
+                  htmlFor="expire_date"
+                  class=""
+                >
+                  Expire Date
+                </label>
+                <input
+                  type="date"
+                  name="expire_date"
+                  id="expire_date"
+                  onInput={(e) => {
+                    const date = (e.target as HTMLInputElement).value;
+                    setExpireDate(date);
+                  }}
+                  class="border-dotted border-black border-2 shadow-lg w-fit px-2 py-1 rounded-md"
+                />
+              </div>
+              <div class="flex flex-col">
+                <label
+                  htmlFor="expire_time"
+                  class=""
+                >
+                  Expire Time
+                </label>
+                <input
+                  type="time"
+                  name="expire_time"
+                  id="expire_time"
+                  onInput={(e) => {
+                    const time = (e.target as HTMLInputElement).value;
+                    setExpireTime(time);
+                  }}
+                  class="border-dotted border-black border-2 shadow-lg w-fit px-2 py-1 rounded-md"
+                />
+              </div>
+            </div>
+          )
+          : <></>}
         <button
           type="button"
           onClick={() => store()}
--- routes/api/url.ts
+++ routes/api/url.ts
@@ -7,8 +7,12 @@ export const handler = async (
   _ctx: FreshContext,
 ): Promise<Response> => {
   const alias = createAlias(5);
-  const { url } = await req.json() as NewUrl;
-  await kv.set([alias], [url]);
+  const { url, expireTimestamp } = await req.json() as NewUrl;
+  if (expireTimestamp) {
+    await kv.set([alias], [url], { expireIn: expireTimestamp });
+  } else {
+    await kv.set([alias], [url]);
+  }
   return new Response(JSON.stringify({
     alias,
     url,
@@ -17,4 +21,5 @@ export const handler = async (
 
 interface NewUrl {
   url: string;
+  expireTimestamp?: number | null;
 }
